f925dc6a9e4dc88c28cdc9c59e4d1ef60865af89
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2025-02-28 14:35:52 +0100

    ok!

---
+++ site.js
@@ -0,0 +1,49 @@
+const menuItems = document.querySelectorAll('.menu-item');
+let currentIndex = 0;
+
+menuItems[currentIndex].classList.add('active');
+
+// if i press the arrow keydown do something
+document.addEventListener("keydown", (e) => {
+
+
+  if (e.key === 'ArrowDown') {
+    handleKeyDown();
+  }
+
+  if (e.key === 'ArrowUp') {
+    handleKeyUp();
+  }
+
+});
+
+function handleKeyUp() {
+  currentIndex = currentIndex - 1;
+
+  menuItems[currentIndex + 1].classList.remove('active');
+
+  removeAllOtherActive();
+  menuItems[currentIndex].classList.add('active');
+  console.log(`The current index is: ${currentIndex}`);
+  console.log(menuItems[currentIndex]);
+}
+
+
+function handleKeyDown() {
+  currentIndex = currentIndex + 1;
+
+  menuItems[currentIndex - 1].classList.remove('active');
+
+  removeAllOtherActive();
+  menuItems[currentIndex].classList.add('active');
+  console.log(`The current index is: ${currentIndex}`);
+  console.log(menuItems[currentIndex]);
+}
+
+function removeAllOtherActive() {
+  menuItems.forEach((item) => {
+    item.classList.remove('active');
+  });
+}
+
+
