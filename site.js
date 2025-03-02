ca4856b7fa918b53997a0f62277e8e0640e82dad
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2025-03-02 22:58:16 +0100

    the fix

--- site.js
+++ site.js
@@ -6,7 +6,6 @@ menuItems[currentIndex].classList.add('active');
 // if i press the arrow keydown do something
 document.addEventListener("keydown", (e) => {
 
-
   if (e.key === 'ArrowDown') {
     handleKeyDown();
   }
@@ -18,11 +17,20 @@ document.addEventListener("keydown", (e) => {
 });
 
 function handleKeyUp() {
+
+  if (currentIndex === 0) {
+    currentIndex = menuItems.length - 1;
+    removeAllOtherActive();
+    menuItems[currentIndex].classList.add('active');
+    return
+  }
+
   currentIndex = currentIndex - 1;
 
   menuItems[currentIndex + 1].classList.remove('active');
 
   removeAllOtherActive();
+
   menuItems[currentIndex].classList.add('active');
   console.log(`The current index is: ${currentIndex}`);
   console.log(menuItems[currentIndex]);
@@ -30,11 +38,20 @@ function handleKeyUp() {
 
 
 function handleKeyDown() {
+
+  if (currentIndex === menuItems.length - 1) {
+    currentIndex = 0;
+    removeAllOtherActive();
+    menuItems[currentIndex].classList.add('active');
+    return;
+  }
+
   currentIndex = currentIndex + 1;
 
   menuItems[currentIndex - 1].classList.remove('active');
 
   removeAllOtherActive();
+
   menuItems[currentIndex].classList.add('active');
   console.log(`The current index is: ${currentIndex}`);
   console.log(menuItems[currentIndex]);
